0149aacc1eec2bb09a0948786b6498ea542c3752
Jeff <machyo.j@northeastern.edu>
2022-06-21 20:39:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ int main (void)
 {
 	char filename[] = "/home/pi/Documents/Projects/image1.jpg";
 	takePic(filename);
-	//stopPic();
-	cout << "Image successful.."<<endl;
+	cout << "Image successful.."<< endl;
 	return 0;
 }
 
