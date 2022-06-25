7b34725330efa3b28049004f1a4119907779a120
Jeff <machyo.j@northeastern.edu>
2022-06-25 17:18:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main (void) 
 {
+    //Input your desired filepath in place of the filepath denoted here
 	char filename[] = "/home/pi/Documents/Projects/image1.jpg";
 	takePic(filename);
 	cout<<"Image successful.."<<endl;
