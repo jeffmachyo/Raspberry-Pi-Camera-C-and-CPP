2c03db4a2200e0362d97bf26ea7c2f7b0a3b3870
Jeff <machyo.j@northeastern.edu>
2022-06-25 17:16:25 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 int main (void) 
 {
+    //Input your desired filepath in place of the filepath denoted here
 	char filename[] = "/home/pi/Documents/Projects/image1.jpg";
 	takePic(filename);
 	printf("Image successful..\n");
